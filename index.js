d81b5c2bb7c6deaf52916518ee96af3daf7ccada
NguyenPhuongToan <98100052+NguyenPhuongToan@users.noreply.github.com>
2024-08-01 11:57:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const express = require('express');
-const path = require('path'); // Import the 'path' module
-
 const app = express();
-const port = process.env.PORT || 3000; // Corrected 'port' to uppercase
+const port = process.env.port || 3000
 
-// Serve static files from the 'client/dist' directory
-app.use(express.static(path.join(process.cwd(), 'client', 'dist')));
+app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-  // Send the 'index.html' file
-  res.sendFile(path.join(process.cwd(), 'client', 'dist', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'D:\học tập\web\My_website\index.html')
 });
+app.post('register' , (req,res) =>{
+  const { username, email, password} =req.body;
+  res.json({message: 'User registered successfully'});
 
-app.post('/register', (req, res) => {
-  const { username, email, password } = req.body;
-  res.json({ message: 'User registered successfully' });
-});
+})
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port, ()=>{
+  console.log('Server is running on port ${port}');
 });
